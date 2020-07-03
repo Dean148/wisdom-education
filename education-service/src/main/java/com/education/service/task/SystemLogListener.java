4700634e82f7b8b82154aec44e7dd19e38f19cf3
追梦 <1913188966@qq.com>
2020-07-03 11:12:02 +0800

    日志优化

--- education-service/src/main/java/com/education/service/task/SystemLogListener.java
+++ education-service/src/main/java/com/education/service/task/SystemLogListener.java
@@ -26,8 +26,10 @@ public class SystemLogListener implements TaskListener {
         FrontUserInfoSession frontUserInfoSession = (FrontUserInfoSession) taskParam.get("frontUserInfoSession");
         if (ObjectUtils.isNotEmpty(adminUserSession)) {
             taskParam.put("user_id", adminUserSession.getUserMap().get("id"));
+            taskParam.put("operation_name", adminUserSession.getUserMap().get("login_name"));
         } else if (ObjectUtils.isNotEmpty(frontUserInfoSession)) {
             taskParam.put("user_id", frontUserInfoSession.getUserInfoMap().get("id"));
+            taskParam.put("operation_name", frontUserInfoSession.getUserInfoMap().get("login_name"));
         }
         taskParam.put("create_date", new Date());
         taskParam.put("platform_type", EnumConstants.PlatformType.WEB_ADMIN.getValue());
