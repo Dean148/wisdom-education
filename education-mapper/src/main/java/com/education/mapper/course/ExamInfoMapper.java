79579c225bb9708d03db385639471002d712e717
追梦 <1913188966@qq.com>
2020-07-07 11:21:14 +0800

    删除试卷

--- education-mapper/src/main/java/com/education/mapper/course/ExamInfoMapper.java
+++ education-mapper/src/main/java/com/education/mapper/course/ExamInfoMapper.java
@@ -2,6 +2,8 @@ package com.education.mapper.course;
 
 import com.education.common.base.BaseMapper;
 import com.education.common.model.ModelBeanMap;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
@@ -25,4 +27,7 @@ public interface ExamInfoMapper extends BaseMapper {
      * @return
      */
     List<ModelBeanMap> countByDateTime(Map params);
+
+    @Select("select * from exam_info where test_paper_info_id = #{paperId} limit 1")
+    ModelBeanMap findByTestPaperId(@Param("paperId") Integer paperId);
 }
