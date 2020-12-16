116119c48ac1c46568c790ca45bdd957320269d2
1913188966@qq.com <1913188966@qq.com>
2020-12-16 11:04:48 +0800

    删除无用代码

--- education-api/src/main/java/com/education/api/controller/student/WrongBookController.java
+++ education-api/src/main/java/com/education/api/controller/student/WrongBookController.java
@@ -1,11 +1,7 @@
 package com.education.api.controller.student;
 
 import com.education.business.service.education.StudentWrongBookService;
-import com.education.business.task.TaskManager;
-import com.education.business.task.TaskParam;
-import com.education.business.task.WebSocketMessageTask;
 import com.education.common.base.BaseController;
-import com.education.common.constants.EnumConstants;
 import com.education.common.model.PageInfo;
 import com.education.common.utils.Result;
 import com.education.model.dto.QuestionInfoAnswer;
@@ -29,10 +25,6 @@ public class WrongBookController extends BaseController {
     @Autowired
     private StudentWrongBookService studentWrongBookService;
 
-    @Autowired
-    private TaskManager taskManager;
-
-
     /**
      * 错题本列表
      * @param pageParam
@@ -42,12 +34,6 @@ public class WrongBookController extends BaseController {
     @GetMapping
     public Result<PageInfo<QuestionInfoAnswer>> list(PageParam pageParam, WrongBookQuery wrongBookQuery) {
         wrongBookQuery.setStudentId(studentWrongBookService.getStudentInfo().getId());
-
-
-        TaskParam taskParam = new TaskParam(WebSocketMessageTask.class);
-        taskParam.put("message_type", EnumConstants.MessageType.EXAM_CORRECT.getValue());
-        taskParam.put("sessionId", "99462b64-9f1d-4c0e-86bf-4256540880ba");
-        taskManager.pushTask(taskParam);
         return Result.success(studentWrongBookService.selectPageList(pageParam, wrongBookQuery));
     }
 }
