ab9d53ac12a78b558cf7f3a1c1d996bfea8a76a9
xyl； <3212401556@qq.com>
2021-05-08 11:14:37 +0800

    quartz job 持久化功能

---
+++ education-api/src/main/java/com/education/api/controller/admin/system/QuartzController.java
@@ -0,0 +1,33 @@
+package com.education.api.controller.admin.system;
+
+import com.education.business.service.system.SystemQuartzJobService;
+import com.education.common.base.BaseController;
+import com.education.common.utils.Result;
+import com.education.model.entity.SystemQuartzJob;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 定时任务管理
+ */
+@RestController
+@RequestMapping("/system/quartz")
+public class QuartzController extends BaseController {
+
+    @Autowired
+    private SystemQuartzJobService systemQuartzJobService;
+
+    /**
+     * 添加定时任务
+     * @param systemQuartzJob
+     * @return
+     */
+    @PostMapping("save")
+    public Result saveQuartz(@RequestBody SystemQuartzJob systemQuartzJob) {
+        systemQuartzJobService.saveQuartzJob(systemQuartzJob);
+        return Result.success();
+    }
+}
