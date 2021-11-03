a83369e068ddb2b213de0590e14ad97eba99ee1e
zjt <1719990136@qq.com>
2021-11-03 10:02:17 +0800

    网站配置接口

--- education-api/src/main/java/com/education/api/controller/admin/system/WebSiteConfigController.java
+++ education-api/src/main/java/com/education/api/controller/admin/system/WebSiteConfigController.java
@@ -1,17 +1,11 @@
 package com.education.api.controller.admin.system;
 
-import cn.hutool.core.util.StrUtil;
 import com.education.business.service.system.WebsiteConfigService;
-import com.education.business.webSocket.WebSocketConfig;
 import com.education.common.base.BaseController;
-import com.education.common.exception.BusinessException;
-import com.education.common.utils.ObjectUtils;
 import com.education.common.utils.Result;
 import com.education.model.entity.WebsiteConfig;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author zengjintao
@@ -32,15 +26,6 @@ public class WebSiteConfigController extends BaseController {
 
     @PostMapping
     public Result saveOrUpdate(@RequestBody WebsiteConfig websiteConfig) {
-        List<String> list = websiteConfig.getCarouselImageList();
-        if (list.size() > 3) {
-            throw new BusinessException("最多只能上传三张轮播图");
-        }
-        StringBuilder carouselImage = new StringBuilder();
-        for (String image : list) {
-            carouselImage.append(image).append(StrUtil.COMMA);
-        }
-        websiteConfig.setCarouselImage(carouselImage.substring(0, carouselImage.length() - 1));
         websiteConfigService.saveOrUpdate(websiteConfig);
         return Result.success();
     }
--- education-business/src/main/java/com/education/business/service/system/WebsiteConfigService.java
+++ education-business/src/main/java/com/education/business/service/system/WebsiteConfigService.java
@@ -1,10 +1,14 @@
 package com.education.business.service.system;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.education.business.mapper.system.WebsiteConfigMapper;
 import com.education.business.service.BaseService;
+import com.education.common.exception.BusinessException;
 import com.education.model.entity.WebsiteConfig;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 网站设置
@@ -15,7 +19,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class WebsiteConfigService extends BaseService<WebsiteConfigMapper, WebsiteConfig> {
 
-    public Object getWebSiteConfig() {
+    public WebsiteConfig getWebSiteConfig() {
         return super.selectFirst(new QueryWrapper<>());
+    }
+
+    @Override
+    public boolean saveOrUpdate(WebsiteConfig websiteConfig) {
+        List<String> list = websiteConfig.getCarouselImageList();
+        if (list.size() > 3) {
+            throw new BusinessException("最多只能上传三张轮播图");
+        }
+        StringBuilder carouselImage = new StringBuilder();
+        list.forEach(image -> carouselImage.append(image).append(StrUtil.COMMA));
+        websiteConfig.setCarouselImage(carouselImage.substring(0, carouselImage.length() - 1));
+        return super.saveOrUpdate(websiteConfig);
     }
 }
